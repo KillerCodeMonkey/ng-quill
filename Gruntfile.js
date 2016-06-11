6129e96505d3a8bf460d215b02555e4935dca68c
Bengt Weiße <bengtler@gmail.com>
2016-06-11 14:50:47 +0200

    add umd wrapper

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,13 +6,27 @@ module.exports = function (grunt) {
         uglify: {
             my_target: {
                 files: {
-                    "src/ng-quill.min.js": "src/ng-quill.js"
+                    "src/ng-quill.min.js": "src/ng-quill.min.js"
+                }
+            }
+        },
+        umd: {
+            all: {
+                options: {
+                    src: 'src/ng-quill.js',
+                    dest: 'src/ng-quill.min.js', // optional, if missing the src will be used
+                    deps: { // optional, `default` is used as a fallback for rest!
+                        'default': ['Quill', 'angular'],
+                        amd: ['Quill', 'angular'],
+                        cjs: ['Quill', 'angular']
+                    }
                 }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-umd');
 
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['umd', 'uglify']);
 };
