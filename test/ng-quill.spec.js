2a45ea32ccc85547a6bd7b0dcbf738c27f9f4f8c
Benny Tran <tran.benny123@gmail.com>
2016-08-29 12:07:07 -0700

    added tests for validating properties in ngQuillConfig Provider and ngQuillService

--- test/ng-quill.spec.js
+++ test/ng-quill.spec.js
@@ -1,6 +1,53 @@
 describe('ng-quill-component', function() {
 
+	beforeEach(module('ngQuill'));
 
+	describe('ngQuillConfigProvider', function() {
+		var config;
 
+		beforeEach(inject(function(_ngQuillConfig_) {
+			config = _ngQuillConfig_;
+		}));
+
+		it('should load default properties', function() {
+				var properties = Object.keys(config);
+				expect(config).toBeDefined();
+				expect(properties).toContain('fontSizes');
+				expect(properties).toContain('fontFamilies');
+				expect(properties).toContain('formats');
+				expect(properties).toContain('translations');
+		});
+	});
+
+	describe('ngQuillService', function() {
+		var config; 
+		var service;
+
+		beforeEach(inject(function($injector) {
+			config = $injector.get('ngQuillConfig');
+			service = $injector.get('ngQuillService');
+		}));
+
+		it('should load default config dependency', function() {
+			expect(config).toBeDefined();
+			expect(service).toBeDefined();
+			expect(Object.keys(config).length).toBe(4);
+			expect(service.validateFormats).toBeDefined();
+		});
+
+		it('should load custom list of formats', function() {
+			var sampleFormats = ['link', 'image', 'background', 'align'];
+			expect(service.validateFormats(sampleFormats)).toEqual(sampleFormats);
+		});
+
+		it('should not load invalid configurations', function() {
+			var invalidFormats = ['Link', "ailgn", 'backround', 'Strike', 'circle'];
+			expect(service.validateFormats(invalidFormats)).toEqual([]);
+		});
+	});
+
+	describe('ngQuillEditor', function() {
+
+	});
 
 });
