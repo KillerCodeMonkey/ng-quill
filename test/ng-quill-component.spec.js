caef013dbb9b480006e25a5cf4abb6dc60a98b09
Benny Tran <tran.benny123@gmail.com>
2016-08-30 11:05:50 -0700

    added tests for configuring properties in component

--- test/ng-quill-component.spec.js
+++ test/ng-quill-component.spec.js
@@ -1,4 +1,6 @@
-describe('ng-quill-component', function() {
+// tests for ng-quill-editor component
+
+describe('ng-quill-editor', function() {
 	
 	beforeEach(module('ngQuill'));
 
@@ -8,70 +10,174 @@ describe('ng-quill-component', function() {
 		var $compile;
 		var $rootScope;
 		var $componentController;
-		var $controller;
 
-		// TODO: ADD TEMPLATE AS A VARIABLE HERE
-		var invalidFormat = angular.element("<ng-quill-editor></ng-quill-editor>");
-		var validFormat = angular.element('<ng-quill-editor name="editor1" callback="editorCallback(editor, name)" ' + 
-			'ng-model="message" translations="translations" toolbar="true" show-toolbar="showToolbar" ' + 
-			'link-tooltip="true" image-tooltip="true" toolbar-entries="font size bold list bullet italic ' +
-			'underline strike align color background link image" editor-required="true" required="" ' +
-			'read-only="isReadonly()" error-class="input-error" fontsize-options="fontsizeOptions" ' + 
-			'fontfamily-options="fontfamilyOptions"></ng-quill-editor>');
-
-		beforeEach(inject(function(_$compile_, _$rootScope_, _$controller_, _$componentController_) {
+		
+		var invalidComponent = angular.element("<ng-quill-editor></ng-quill-editor>");
+		var validComponent = angular.element(
+			['<ng-quill-editor name="editor1" callback="editorCallback(editor, name)" ',
+			'ng-model="message" translations="translations" toolbar="true" show-toolbar="showToolbar" ', 
+			'link-tooltip="true" image-tooltip="true" toolbar-entries="font size bold list bullet italic ',
+			'underline strike align color background link image" editor-required="true" required="" ',
+			'read-only="isReadonly()" error-class="input-error" fontsize-options="fontsizeOptions" ', 
+			'fontfamily-options="fontfamilyOptions"></ng-quill-editor>'].join('')
+		);
+		
+		beforeEach(inject(function(_$compile_, _$rootScope_, _$componentController_) {
 			$compile = _$compile_;
 			$rootScope = _$rootScope_;
-			$controller = _$controller_;
 			$componentController = _$componentController_;
 		}));
 
-		describe('creates component correctly', function() {
+		describe('Creates component correctly', function() {
 
 			it('should not create componenet without required attributes and configuration', function() {
-				// use a try catch block to capture the error when compiling 
-				var elem = $compile(invalidFormat)($rootScope);
-				var divElements = elem.find('div');
-				var spanElements = elem.find('span');
-				expect(divElements.length).toBe(0);
-				expect(spanElements.length).toBe(0);
+				var error;
+				try {
+					var elem = $compile(invalidComponent)($rootScope);
+					$rootScope.$digest();
+				} catch(err) {
+					error = "cannot compile component";
+				}
+				expect(error).toBeDefined();
+				expect(error).toBe("cannot compile component");
 			});
 
 			it('should compile component', function() {
-				var elem = $compile(validFormat)($rootScope);
+				var elem = $compile(validComponent)($rootScope);
 				$rootScope.$digest();
 				var divElements = elem.find('div');
 				var spanElements = elem.find('span');
 				expect(divElements.length).not.toBe(0);
 				expect(spanElements.length).not.toBe(0);
 			});
-		});
 
-		describe('handles changing config options correctly', function() {
+			it('should be able to edit text after creating component', function() {
+				var elem = $compile(validComponent)($rootScope);
+				$rootScope.$digest();
+				var inputElement = elem.find('input');
+				// should include one input element with ng-valid class
+				var containsValidInput = false;
+				for (var i = 0; i < inputElement.length; i++) {
+					if (inputElement[i].classList.contains('ng-valid')) {
+						containsValidInput = true;
+					}
+				}
+				expect(containsValidInput).toBe(true);
+			});
+		});
+		
+		describe('ngQuill Component Controller', function() {
 			
-			var element; // html element
-			var ctrl;
-			var $scope; // this is a generic scope object right now
+			var element; 
+			var scope; 
 
 			beforeEach(function() {
-				$scope = $rootScope.$new();
-				element = $compile(validFormat)($scope);
-				
+				scope = $rootScope.$new();
+				element = $compile(validComponent)(scope);
+				scope.$digest();
 			});
 
-			it('sample', function() {
-				ctrl = $componentController('ngQuillEditor', {
-					$scope: $scope,
-					$element: element
-				}); // access to the component's controller 
-				// console.log(element);
-				// console.log($scope);
-				console.log(ctrl); // check that all bindings are correctly set 
-				console.log($scope.toolbar);
+			describe('Custom Bindings', function() {
+				
+				var componentBindings = {
+					'toolbar': true,
+					'link-tooltip': true,
+					'image-tooltip': true,
+					'toolbar-entries': ['font', 'size', 'bold', 'list', 'bullet', 'italic', 'underline',
+					'strike', 'align', 'color', 'background', 'link', 'image'],
+					'editor-required': true,
+					'required': '',
+					'error-class': 'input-error'
+				};
+
+				var ctrl;
+
+				beforeEach(function() {
+					ctrl = $componentController('ngQuillEditor', {
+						$scope: scope,
+						$element: element
+					}, componentBindings);
+				});
+
+				it('should attach controller to component', function() {
+					expect(ctrl).toBeDefined();
+					expect(ctrl).toBe(scope.$ctrl);
+				});
+
+				it('should have attached all bindings to controller', function() {
+					expect(ctrl['toolbar']).toBeDefined();
+					expect(ctrl['link-tooltip']).toBeDefined();
+					expect(ctrl['image-tooltip']).toBeDefined();
+					expect(ctrl['toolbar-entries']).toBeDefined();
+					expect(ctrl['editor-required']).toBeDefined();
+					expect(ctrl['required']).toBeDefined();
+					expect(ctrl['error-class']).toBeDefined();
+				});
+
+				it('should attach toolbar options to controller', function() {
+					expect(ctrl['toolbar']).toBe(true);
+					expect(ctrl['link-tooltip']).toBe(true);
+					expect(ctrl['image-tooltip']).toBe(true);
+					expect(ctrl['toolbar-entries']).toEqual(['font', 'size', 'bold', 'list', 'bullet', 'italic', 'underline',
+					'strike', 'align', 'color', 'background', 'link', 'image']);
+					expect(ctrl['editor-required']).toBe(true);
+					expect(ctrl['required']).toBe(false);
+					expect(ctrl['error-class']).toBe('input-error');
+				});
+
+			});
+
+			describe('Empty Bindings', function() {
+				var ctrl;
+
+				beforeEach(function() {
+					ctrl = $componentController('ngQuillEditor', {
+						$scope: scope,
+						$element: element
+					});
+				});
+
+				it('should attach controller to component', function() {
+					expect(ctrl).toBeDefined();
+					expect(ctrl).toBe(scope.$ctrl);
+				});
+
+				it('should set default font families and font sizes without bindings', function() {
+					var defaultFontFamilies = [{
+						label: 'Sans Serif',
+						alias: 'sans-serif'
+					}, {
+						label: 'Serif',
+						alias: 'serif'
+					}, {
+						label: 'Monospace',
+						alias: 'monospace'
+					}];
+					var defaultFontSizes = [{
+						size: '10px',
+						alias: 'small'
+					}, {
+						size: '13px',
+						alias: 'normal'
+					}, {
+						size: '18px',
+						alias: 'large'
+					}, {
+						size: '32px',
+						alias: 'huge'
+					}];
+					expect(ctrl.fontsizeOptions.length).toBe(4);
+					expect(ctrl.fontfamilyOptions.length).toBe(3);
+					defaultFontFamilies.forEach(function(x) {
+						expect(ctrl.fontfamilyOptions).toContain(jasmine.objectContaining(x));
+					});
+					defaultFontSizes.forEach(function(x) {
+						expect(ctrl.fontsizeOptions).toContain(jasmine.objectContaining(x));
+					});
+				});
+
 			});
 
 		});
-
-
 	});
 });
